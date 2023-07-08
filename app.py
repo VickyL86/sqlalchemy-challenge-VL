6cb46f6323f4ab96e87f6290cb11efd46c1d0d7f
VickyL86 <lasotaludwika1@gmail.com>
2023-07-08 11:18:40 -0500

    Added ibitiak set up for the app

--- app.py
+++ app.py
@@ -1,30 +1,104 @@
 # Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+base = automap_base()
 
 # reflect the tables
-
+base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = base.classes.measurement
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/stations"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    """Return a list of all passenger names"""
+    # Query all measurements
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    
+    session.close()
+    
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+    
+    return jsonify(all_names)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    """Return a list of all temps"""
+    # Query all measurements
+    results = session.query(Measurement.date, Measurement.tobs).all()
+    
+    session.close()
+    
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+    
+    return jsonify(all_names)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    """Return a list of all stations"""
+    # Query all measurements
+    results = session.query(Measurement.name, Measurement.latitutude, Measurement.longitude).all()
+    
+    session.close()
+    
+   # Create a dictionary from the row data and append to a list of all_stations
+    all_stations = []
+    for station in results:
+        station_dict = {}
+        station_dict["station"] = station
+        all_stations.append(station_dict)
+
+    return jsonify(all_stations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
